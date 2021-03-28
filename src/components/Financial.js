9a95ae26d2e33c4e242470a47d70225477e2e181
Alex Nordin <apnordin@gmail.com>
2021-03-27 21:51:34 -0400

    Rewrote finbtns as a loop

--- src/components/Financial.js
+++ src/components/Financial.js
@@ -2,6 +2,17 @@ import React from "react";
 import { Truck } from "react-bootstrap-icons";
 
 export default function Financial () {
+
+    const finBtnClicked = () => {
+
+    }
+
+    const finbtns = [
+        { name: "job", icon: <Truck size={60} />, text: "Had a job", subText: "(W-2)" },
+        { name: "charity" },
+        { name: "college" },
+    ]
+
     return (
         <div className="col-12 text-center">
             <h2 className="welcome-back">Let's get an idea of your financial picture
@@ -10,13 +21,16 @@ export default function Financial () {
             </h5>
             <div className="row">
                 <div className="col-12 text-center mt-4">
-                    <div className="btn btn-finance">
-                        <div className="btnFinIcon">
-                            <Truck size={60} />
-                        </div>
-                        <div className="btnFinText">Had a job</div>
-                        <div className="btnFinSubText">(W-2)</div>
-                    </div>
+                    { finbtns.map(function(finbtn, i) {
+                        return (<div className="btn btn-finance" key={finbtn.name } index={ i }>
+                                <div className="btnFinIcon">
+                                    {finbtn.icon}
+                                </div>
+                                <div className="btnFinText">{finbtn.text}</div>
+                                <div className="btnFinSubText">{finbtn.subText}</div>
+                            </div>
+                            )
+                    })}
                 </div>
             </div>
         </div>
