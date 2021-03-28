20d9f7f04cc7ac145d1d4b5f0f8940117ba94584
Alex Nordin <apnordin@gmail.com>
2021-03-27 21:36:44 -0400

    Fixed errors in page directions, added/formatted finbtns

--- src/components/Financial.js
+++ src/components/Financial.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Truck } from "react-bootstrap-icons";
 
 export default function Financial () {
     return (
         <div className="col-12 text-center">
-            <h5 className="welcome-back">Financial
+            <h2 className="welcome-back">Let's get an idea of your financial picture
+            </h2>
+            <h5 className="subheader">Select anything that applied in 2020.
             </h5>
+            <div className="row">
+                <div className="col-12 text-center mt-4">
+                    <div className="btn btn-finance">
+                        <div className="btnFinIcon">
+                            <Truck size={60} />
+                        </div>
+                        <div className="btnFinText">Had a job</div>
+                        <div className="btnFinSubText">(W-2)</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -3,9 +3,17 @@ import React from "react";
 export default function Tutorial ( {setGoToFinancial, setGoToTutorial }) {
     return (
         <div className="col-12 text-center">
-            <h5 className="welcome-back">Tutorial
-            </h5>
-            <button onClick={() => {setGoToFinancial(true); setGoToTutorial(false);}} type="button" className="btn btn-main mt-3">Start!</button>
+            <h2 className="welcome-back">Instructions
+            </h2>
+            <div className="row">
+                <div className="col-12 text-center lightblue mt-4">
+                    <h5 className="maintexth5 mb-3">Follow the instructions on each page as if you were actually trying to use a tax-filing service. At the end of the simulation, we will review each dark pattern, and you will be able to select which ones you noticed while participating.</h5>
+                </div>
+                <div className="col-12 text-center">
+                    <button onClick={() => {setGoToFinancial(false); setGoToTutorial(false);}} type="button" className="btn btn-grey mr-1 mt-3">Go Back</button>
+                    <button onClick={() => {setGoToFinancial(true); setGoToTutorial(false);}} type="button" className="btn btn-main ml-1 mt-3">Start!</button>
+                </div>
+            </div>
         </div>
     )
 }
